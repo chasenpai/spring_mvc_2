ec62e70501de8cc65e68763ed7880f60d1f909fd
junwvwv <wvwv0612@gmail.com>
2023-07-08 20:23:35 +0900

    체크박스

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,16 +3,20 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
@@ -27,18 +31,22 @@ public class FormItemController {
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
+        //model.addAttribute("regions", regions());
         model.addAttribute("item", item);
         return "form/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
+       // model.addAttribute("regions", regions());
         model.addAttribute("item", new Item());
         return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.isOpen()); //스프링 타입 컨버터가 on > true 로 변환
+        log.info("regions={}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -48,6 +56,7 @@ public class FormItemController {
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
+        //model.addAttribute("regions", regions());
         model.addAttribute("item", item);
         return "form/editForm";
     }
@@ -56,6 +65,20 @@ public class FormItemController {
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
+    }
+
+    /**
+     * @ModelAttribute 의 특별한 사용법
+     * - 컨트롤러에 있는 별도의 메서드에도 적용할 수 있다
+     * - 해당 컨트롤러를 요청할 때 regions 에서 반환한 값이 자동으로 Model 에 담긴다
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
     }
 
 }
