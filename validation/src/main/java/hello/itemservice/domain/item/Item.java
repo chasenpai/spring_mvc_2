88200728b38e67890573714b7d7e68eb9ecb1985
junwvwv <wvwv0612@gmail.com>
2023-07-18 20:09:43 +0900

    groups

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,24 +2,49 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//java 17 부터 사용 불가
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank //빈값 + 공백만 있는 경우를 허용하지 않는다
+
+    /**
+     * Bean Validation 메세지 찾는 순서
+     * 1. 생성된 메세지 코드 순서대로 messageSource 에서 찾기
+     * @NotBlank
+     * NotBlank.item.itemName
+     * NotBlank.itemName
+     * NotBlank.java.lang.String
+     * NotBlank
+     * @Range
+     * Range.item.price
+     * Range.price
+     * Range.java.lang.Integer
+     * Range
+     * 2. 어노테이션의 message 속성 사용
+     * 3. 라이브러리가 제공하는 기본 값 사용
+     */
+
+    //빈값 + 공백만 있는 경우를 허용하지 않는다
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull //null 을 허용하지 않는다
-    @Range(min = 1000, max = 1000000) //범위 안의 값이어야 한다
+    //null 을 허용하지 않는다
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //범위 안의 값이어야 한다
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999) //최대 9999까지 허용한다
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class}) //최대 9999까지 허용한다
     private Integer quantity;
 
     public Item() {
--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -49,9 +51,40 @@ public class ValidationItemControllerV3 {
      * - @Validated 는 스프링 전용, @Valid 는 자바 표준 검증 어노테이션이다.
      * - 바인딩에 성공한 필드만 Bean Validation 을 적용한다
      */
-    @PostMapping("/add")
-    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+    //@PostMapping("/add")
+    public String addItemV1(@Validated @ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes, Model model) {
+
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@Validated(SaveCheck.class) //groups 적용 - @Validated 어노테이션만 지원
+                            //groups 기능 사용 시 등록과 수정 시 각각 다르게 검증할 수 있다. 하지만 전반저으로 복잡도가 올라간다
+                            @ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes, Model model) {
+
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
 
         if(bindingResult.hasErrors()){
             log.info("errors = {}", bindingResult);
@@ -71,8 +104,41 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
+    //@PostMapping("/{itemId}/edit")
+    public String editV1(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String editV2(@PathVariable Long itemId, @Validated(UpdateCheck.class) ////groups 적용
+                         @ModelAttribute Item item, BindingResult bindingResult) {
+
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
