e21a2134d84cd0451fd59f43353f40e0e83d56cd
junwvwv <wvwv0612@gmail.com>
2023-07-28 17:39:10 +0900

    서블릿 예외 처리 - 인터셉터, 필터 BasicErrorController

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,8 +6,19 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    /**
+     * 스프링 부트는 예외 처리 페이지를 간편하게 구현하기 위한 BasicErrorController 를 제공한다
+     * BasicErrorController 가 제공하는 룰과 우선순위에 따라서 오류 페이지를 등록하면 된다
+     * 오류가 발생하면 오류 페이지로 /error 를 기본으로 요청한다
+     *
+     * 뷰 선택 우선순위
+     * 1. 뷰 템플릿 - resources/templates/error/500.html
+     * 2. 정적 리소스 - resources/static/error/400.html
+     * 3. 적용 대상이 없을 때 뷰이름 error - resources/templates/error.html
+     */
 
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
